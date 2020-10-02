7033af48974041458250c6adab8929e577b9b647
Julia Kennard <juliabkennard@gmail.com>
2020-10-02 09:51:21 -0700

    Deleted bonus from functions_intermediate1

--- functions_intermediate1.py
+++ functions_intermediate1.py
@@ -8,12 +8,3 @@ print(randInt())
 print(randInt(max = 50))
 print(randInt(min = 50))
 print(randInt(min = 50, max = 500))
-
-# BONUS: Accounting for egde cases
-
-import random
-def randInt(min = 0, max = 100):
-    num = random.random() * (max - min) + min
-    return num
-
-print(randInt(max = -300))
