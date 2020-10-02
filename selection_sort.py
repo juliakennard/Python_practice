190f3e811a1d7eed7e7b205f0e38157285bc36bb
Julia Kennard <juliabkennard@gmail.com>
2020-10-01 20:14:54 -0700

    selection_sort.py

---
+++ selection_sort.py
@@ -0,0 +1,13 @@
+my_array = [4,3,2,1,2]
+
+def selection_sort(some_list):
+    for j in range(0, len(some_list) - 1):
+        min = some_list[j]
+        for i in range(j, len(some_list)):
+            if some_list[i] < min:
+                min = some_list[i]
+                some_list[i], some_list[j] = some_list[j], some_list[i]
+    return some_list
+
+minimum = (selection_sort(my_array))
+print(minimum)
